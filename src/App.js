c89b233ba258bd491e510a8512f5a0b2c3c9f6fc
Ankit Kumar <87308865+ankitrajput30@users.noreply.github.com>
2022-05-15 02:27:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant';
+import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+import Todo from './component/todoReact/todo';
+import Temp from './component/weather/temp';
+import Weathercard from './component/weather/weathercard';
+
+const App = () => {
+  return <Temp />;
+};
+
+export default App
+ 
